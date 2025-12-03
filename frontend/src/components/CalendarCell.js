2ef2b694a74f1437d3b5f980d36af0aeeae213e9
Aldris <alicja.j.przybylska@gmail.com>
2025-12-03 15:38:22 +0100

    Make calendar tabable (#27)

--- frontend/src/components/CalendarCell.js
+++ frontend/src/components/CalendarCell.js
@@ -1,14 +1,22 @@
 export default function CalendarCell(props) {
+    const isTabbable = props.day && !props.openPanel;
+
     return (
         <div
             className={props.day && "calendar-cell"}
             onClick={props.onClick}
+            tabIndex={isTabbable ? 0 : -1}
+            onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                    props.onClick();
+                }
+            }}
         >
             <div className={props.day && "calendar-cell-number"}>
                 {props.day}
             </div>
             <div className="calendar-cell-content">
-                
+
             </div>
         </div>
     );
--- frontend/src/components/CalendarSidePanel.js
+++ frontend/src/components/CalendarSidePanel.js
@@ -2,13 +2,23 @@ import { ReactComponent as Arrow } from "../assets/arrow-right.svg";
 
 export default function CalendarSidePanel(props) {
     return (
-        <div className={`side-panel ${props.openPanel ? "open" : ""}`}>
+        <div
+            className={`side-panel ${props.openPanel ? "open" : ""}`}
+            tabIndex={props.openPanel ? 0 : -1}>
             <div className="side-panel-header">
                 <h1>{props.date}</h1>
             </div>
             <div className="side-panel-content">
                 <div className="side-panel-content-header">
-                    <Arrow className="arrow-icon arrow-icon-left" onClick={props.onClick} />
+                    <Arrow
+                        className="arrow-icon arrow-icon-left"
+                        onClick={props.onClick}
+                        tabIndex={0}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" || e.key === " ") {
+                                props.onClick();
+                            }
+                        }} />
                     <h2>Twoje zadania</h2>
                     <button className="edit-icon">Placeholder</button>
                 </div>
