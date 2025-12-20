4eba8467ddec4fcac4c8007436c8898a82f18db2
Aldris <33839068+ItsAldris@users.noreply.github.com>
2025-12-20 16:17:50 +0100

    Redirect from calendar to todo page

--- frontend/src/components/Calendar/CalendarSidePanel.js
+++ frontend/src/components/Calendar/CalendarSidePanel.js
@@ -1,20 +1,34 @@
 import { ReactComponent as Arrow } from "../../assets/arrow-right.svg";
 import { ReactComponent as EditButton } from "../../assets/edit-button.svg";
 import Task from "../Todo/Task";
+import { useNavigate } from "react-router-dom";
+import { getDateString } from "../../functions/TextFunctions";
+
 
 export default function CalendarSidePanel(props) {
+    const navigate = useNavigate();
+    const redirectTodo = () => {
+        navigate("/todo?date=" + getDateString(props.date));
+    }
+
     return (
         <div
             className={`side-panel ${props.openPanel ? "open" : ""}`}
             tabIndex={props.openPanel ? 0 : -1}>
             <div className="side-panel-header">
-                <h1>{props.date}</h1>
+                <h1>{props.date?.toLocaleString("default", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric"
+                })}</h1>
             </div>
             <div className="side-panel-content">
                 <div className="side-panel-content-header">
                     <Arrow
                         className="arrow-icon arrow-icon-left"
-                        onClick={props.onClick}
+                        onClick={() => {
+                            props.onClick();
+                        }}
                         tabIndex={0}
                         onKeyDown={(e) => {
                             if (e.key === "Enter" || e.key === " ") {
@@ -25,6 +39,14 @@ export default function CalendarSidePanel(props) {
                     <EditButton
                         className="edit-icon"
                         tabIndex={0}
+                        onClick={() => {
+                            redirectTodo();
+                        }}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" || e.key === " ") {
+                                redirectTodo();
+                            }
+                        }}
                     />
                 </div>
                 {props.tasks.length > 0 ?
@@ -37,7 +59,7 @@ export default function CalendarSidePanel(props) {
                     </div>
                     :
                     <div className="calendar-no-data-text">
-                        <p>Nie dodano jeszcze żadnych zadań do&nbsp;tego dnia. Kliknij na&nbsp;ikonę <EditButton/> w&nbsp;prawym górnym rogu lub przejdź do&nbsp;zakładki ToDo, aby dodać nowe zadania.</p>
+                        <p>Nie dodano jeszcze żadnych zadań do&nbsp;tego dnia. Kliknij na&nbsp;ikonę <EditButton /> w&nbsp;prawym górnym rogu lub przejdź do&nbsp;zakładki ToDo, aby dodać nowe zadania.</p>
                     </div>
                 }
 
