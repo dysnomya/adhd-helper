04d5f7e128d42cad668d407890787723ec143def
Piotr Natkański <72403838+Su9in@users.noreply.github.com>
2025-12-22 21:52:20 +0100

    Update Split.js

--- frontend/src/pages/Split.js
+++ frontend/src/pages/Split.js
@@ -22,8 +22,8 @@ const Split = () => {
 
 
     // Inicjalizacja Gemini (Klucz API najlepiej trzymać w .env)
-    // const genAI = new GoogleGenerativeAI("AIzaSyA1UoiLF7bVwrKgw1sgx-fo_E7KDmbakgA");
-    const genAI = new GoogleGenerativeAI("AIzaSyASUp9UVlusUXn-_wL_K9Fk_NkST9UZbSg");
+    // const genAI = new GoogleGenerativeAI("");
+    const genAI = new GoogleGenerativeAI("");
     const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const SplitTaskButtonClicked = async () => {
