405ca53c5f4d28bbfb102ff53963568530963a7c
ngackowska <ngackowska02@gmail.com>
2025-12-22 01:52:04 +0100

    Added task options panel

--- frontend/src/components/Todo/Task.js
+++ frontend/src/components/Todo/Task.js
@@ -4,11 +4,17 @@ import { timeDisplay } from "../../functions/TasksHelpers";
 import { ReactComponent as Clock} from "../../assets/clock_icon.svg";
 
 import { ReactComponent as CheckIcon } from "../../assets/check_icon.svg";
+import { ReactComponent as TriangleArrowIcon } from "../../assets/triangle-arrow-button.svg";
+
+import { ReactComponent as EditIcon } from "../../assets/edit-button.svg";
+import { ReactComponent as SplitIcon } from "../../assets/split-button.svg";
+import { ReactComponent as DeleteIcon } from "../../assets/delete-button.svg";
 
 const Task = ({ task, isSubtask = false, onStatusChange }) => {
 
     const [isCompleted, setIsCompleted] = useState(task.completed);   // checkbox
     const [isExpanded, setIsExpanded] = useState(false);    // subtasks
+    const [isExpandedOptions, setIsExpandedOptions] = useState(false);
 
     const subtasks = task.subtasks || [];
     const hasChildren = subtasks.length > 0;
@@ -53,6 +59,23 @@ const Task = ({ task, isSubtask = false, onStatusChange }) => {
         setIsExpanded(!isExpanded);
     };
 
+    const handleExpandOptions = () => {
+        setIsExpandedOptions(!isExpandedOptions);
+    };
+
+
+    const handleOptionClickEdit = () => {
+        console.log("Editing")
+    }
+
+    const handleOptionClickSplit = () => {
+        console.log("Splitting")
+    }
+
+    const handleOptionClickDelete = () => {
+        console.log("Deleting")
+    }
+    
     return (
 
         <div 
@@ -60,50 +83,89 @@ const Task = ({ task, isSubtask = false, onStatusChange }) => {
             style={{ '--cat-color': categoryColor }}
         >
 
-            <div className="task-card">
+            <div className="task-row-wrapper">
 
-                {!isSubtask && (
-                    <div className="task-category-header">
-                        {categoryName}
-                    </div>
-                )}
+                <div className="task-options-panel">
+                    <EditIcon 
+                        className="option-btn edit" 
+                        onClick={handleOptionClickEdit}
+                    />
+
+                    <SplitIcon 
+                        className="option-btn split" 
+                        onClick={handleOptionClickSplit}
+                    />
+
+                    <DeleteIcon 
+                        className="option-btn delete" 
+                        onClick={handleOptionClickDelete}
+                    />
                 </div>
-                <div className="task-content-box">
 
-                    <div className="task-left">
-                        <div 
-                            className={`custom-checkbox ${task.completed ? 'checked' : ''}`}
-                            onClick={handleCheckedTask}
-                        >
-                            {isCompleted && <CheckIcon className="dot"></CheckIcon>}
-                        </div>
+                <div className={`task-swipe-content ${isExpandedOptions ? 'panel-open' : ''}`}>
 
-                        {hasChildren && (
-                            <div 
-                                className={`expand-arrow ${isExpanded ? 'expanded' : ''}`}
-                                onClick={handleToggleSubtask}
-                            >
-                                ›
+                    <div className="task-card">
+
+                        {!isSubtask && (
+                            <div className="task-category-header">
+                                {categoryName}
                             </div>
                         )}
-
-                        <div className="task-info">
-                            <p className={`task-name ${task.completed ? 'completed-text' : ''}`}>
-                                {task.name}
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="task-right">
-                        {task.timeNeeded && (
-                            <span className="task-time">
-                                <span className="clock-icon"><Clock className="clock_icon"></Clock></span> {timeDisplay(task.timeNeeded)}
-                            </span>
+                        {isSubtask && (
+                            <div className="subtask-header">
+                            </div>
                         )}
-                       
                     </div>
+
+                    <div className="task-content-box">
+
+                        
+
+                        <div className="task-left">
+                            <div 
+                                className={`custom-checkbox ${task.completed ? 'checked' : ''}`}
+                                onClick={handleCheckedTask}
+                            >
+                                {isCompleted && <CheckIcon className="dot"></CheckIcon>}
+                            </div>
+
+                            {hasChildren && (
+                                <div 
+                                    className={`expand-arrow ${isExpanded ? 'expanded' : ''}`}
+                                    onClick={handleToggleSubtask}
+                                >
+                                    ›
+                                </div>
+                            )}
+
+                            <div className="task-info">
+                                <p className={`task-name ${task.completed ? 'completed-text' : ''}`}>
+                                    {task.name}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="task-right">
+                            {task.timeNeeded && (
+                                <span className="task-time">
+                                    <span className="clock-icon"><Clock className="clock_icon"></Clock></span> {timeDisplay(task.timeNeeded)}
+                                </span>
+                            )}
+
+                            <TriangleArrowIcon 
+                                className={`task-expand-options ${isExpandedOptions ? 'options-expanded' : ''}`} 
+                                onClick={handleExpandOptions}
+                            />
+                        
+                        </div>
+
+                        
+                    </div>
+                
                 </div>
             
+            </div>
+
 
             {hasChildren && isExpanded && (
                 <div className="subtasks-container">
