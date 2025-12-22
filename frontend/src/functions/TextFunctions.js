bd43678e853622099708e4a97bfdf21f1dd4ae39
Aldris <alicja.j.przybylska@gmail.com>
2025-12-22 20:31:05 +0100

    Redirect from calendar to todo page (#49)

--- frontend/src/functions/TextFunctions.js
+++ frontend/src/functions/TextFunctions.js
@@ -1,3 +1,13 @@
-export default function capitalizeFirstLetter(val) {
+function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
+// YYYY-MM-DD format from Date object in calendar
+function getDateString(date) {
+    return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().split('T')[0];
+}
+
+export {
+  capitalizeFirstLetter,
+  getDateString,
 }
