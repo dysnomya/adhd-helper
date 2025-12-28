40e8550783f14cdbfd973edb9a57c7be58e3a9de
Aldris <33839068+ItsAldris@users.noreply.github.com>
2025-12-28 19:43:26 +0100

    Finish basic styling

--- frontend/src/components/Game/PetView.js
+++ frontend/src/components/Game/PetView.js
@@ -2,13 +2,16 @@ import pimpus from "../../assets/pimpus_happy_anim.webp";
 
 export default function PetView() {
 
-    return(
-        <div>
-            <h1 className="pet-name">Pimpuś</h1>
-            <img src={pimpus} alt="Pimpuś" className="pimpus game-pimpus"/>
-            <div>
-                <h2>Żarcie</h2>
-                <button>Yes</button>
+    return (
+        <div className="game-pet-container">
+            <h1 className="game-pet-name">Pimpuś</h1>
+            <img src={pimpus} alt="Pimpuś" className="pimpus game-pimpus" />
+            <div className="game-food-container">
+                <h2>Dostępne jedzonko: 5</h2>
+                <div className="game-food-buttons-container">
+                    <button className="game-food-button">Daj 1</button>
+                    <button className="game-food-button" disabled={true}>Daj 10</button>
+                </div>
             </div>
         </div>
     );
